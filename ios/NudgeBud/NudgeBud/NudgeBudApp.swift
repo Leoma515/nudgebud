85eab7c248c617d16df95b03aff5af0fddcd3c90
Jerry Moffett <jerry.moffett@outlook.com>
2025-11-06 08:56:57 -0500

    chore: Xcode project scaffold (SwiftUI, iOS 17)

---
+++ ios/NudgeBud/NudgeBud/NudgeBudApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeBudApp.swift
+//  NudgeBud
+//
+//  Created by Jerry Moffett on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct NudgeBudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
